7bcd87d67513e9d5a002d99fa0bd0592fe513d8b
alanndo <134842237+alanndo@users.noreply.github.com>
2023-08-09 21:46:57 -0500

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -74,9 +74,7 @@ class OInventory:
 
 
     def damaged(self):
-        # sort items in to another file that are damaged
         items = self.item_list
-        # get order of keys to write to file based on price
         keys = sorted(items.keys())
         with open('DamagedInventory.csv', 'w') as file:
             for item in keys:
@@ -94,7 +92,7 @@ if __name__ == '__main__':
     items = {}
     files = ['ManufacturerList.csv', 'PriceList.csv', 'ServiceDatesList.csv']
     for file in files:
-        with open(file,"r") as csv_file:
+        with open(file, 'r') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             for line in csv_reader:
                 item_id = line[0]
@@ -114,10 +112,101 @@ if __name__ == '__main__':
                     items[item_id]['service_date'] = service_date
 
     inventory = OInventory(items)
-    # output files
     inventory.full()
     inventory.by_type()
     inventory.past_service()
     inventory.damaged()
 
+    types = []
+    manufacturers = []
+    for item in items:
+        checked_manufacturer = items[item]['manufacturer']
+        checked_type = items[item]['item_type']
+        if checked_manufacturer not in types:
+            manufacturers.append(checked_manufacturer)
+        if checked_type not in types:
+            types.append(checked_type)
+
+    user_input = None
+    while user_input != 'q':
+        user_input = input("\nPlease enter an item manufacturer and item type (ex: Apple laptop) or enter 'q' to quit:\n")
+        if user_input == 'q':
+            break
+        else:
+            selected_manufacturer = None
+            selected_type = None
+            user_input = user_input.split()
+            bad_input = False
+            for word in user_input:
+                if word in manufacturers:
+                    if selected_manufacturer:
+                        bad_input = True
+                    else:
+                        selected_manufacturer = word
+                elif word in types:
+                    if selected_type:
+                        bad_input = True
+                    else:
+                        selected_type = word
+            if not selected_manufacturer or not selected_type or bad_input:
+                print("No such item in inventory")
+            else:
+                
+                keys = sorted(items.keys(), key=lambda x: items[x]['price'], reverse=True)
+
+                
+                matching_items = []
+
+                similar_items = {}
+                for item in keys:
+                    if items[item]['item_type'] == selected_type:
+                        
+                        today = datetime.now().date()
+                        service_date = items[item]['service_date']
+                        service_expiration = datetime.strptime(service_date, "%m/%d/%Y").date()
+                        expired = service_expiration < today
+                        if items[item]['manufacturer'] == selected_manufacturer:
+                            if not expired and not items[item]['damaged']:
+                                matching_items.append((item, items[item]))
+                        else:
+                            if not expired and not items[item]['damaged']:
+                                similar_items[item] = items[item]
+
+                
+                if matching_items:
+                    item = matching_items[0]
+                    item_id = item[0]
+                    man_name = item[1]['manufacturer']
+                    item_type = item[1]['item_type']
+                    price = item[1]['price']
+                    print("Your item is: {}, {}, {}, {}\n".format(item_id, man_name, item_type, price))
+
+                    
+                    if similar_items:
+                        matched_price = price
+                        
+                        closest_item = None
+                        closest_price_diff = None
+                        for item in similar_items:
+                            if closest_price_diff == None:
+                                closest_item = similar_items[item]
+                                closest_price_diff = abs(int(matched_price) - int(similar_items[item]['price']))
+                                item_id = item
+                                man_name = similar_items[item]['manufacturer']
+                                item_type = similar_items[item]['item_type']
+                                price = similar_items[item]['price']
+                                continue
+                            price_diff = abs(int(matched_price) - int(similar_items[item]['price']))
+                            if price_diff < closest_price_diff:
+                                closest_item = item
+                                closest_price_diff = price_diff
+                                item_id = item
+                                man_name = similar_items[item]['manufacturer']
+                                item_type = similar_items[item]['item_type']
+                                price = similar_items[item]['price']
+                        print("You may, also, consider: {}, {}, {}, {}\n".format(item_id, man_name, item_type, price))
+
+                else:
+                    print("No such item in inventory")
+ 
  
